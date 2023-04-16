2c627e77d7af2767a1e0cfc6d2d8867d3cbbacfd
moulindavid <moulin.david@live.fr>
2023-04-16 19:17:17 +0200

    feat: stuff

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
+import fs from "fs";
+
 const randomList = [1, 2, 3].map( x => x + 1);
 
 console.log(randomList);
 
+
+fs.readFileSync("lines").
+    toString().
+    split("\n").
+    forEach(line => console.log(line));
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,27 @@
 fn main() {
+    let foo: Vec<_> = vec![1, 2 ,3]
+        .iter()
+        .map(|x| x + 1)
+        .collect();
 
+    println!("{:?}", foo);
+
+    let data = vec![1, 2, 3];
+    let mut bar = data
+        .iter()
+        .map(|x| x + 1);
+
+    let mut new_vector = vec![];
+
+    while let Some(x) = bar.next() {
+        new_vector.push(x)
+    }
+
+    println!("{:?}", foo);
+
+    let file = std::fs::read_to_string("lines").unwrap();
+   
+    file
+        .lines()
+        .for_each(|line| println!("{}", line));
 }
-
