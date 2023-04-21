7969d630e4d5c739c1709c0aa9f7937af75b6d63
moulindavid <moulin.david@live.fr>
2023-04-21 17:39:15 +0200

    feat: some vector

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,4 @@
-import fs from "fs";
+function practice( nums: number[], index: number): number {
 
-const randomList = [1, 2, 3].map( x => x + 1);
-
-console.log(randomList);
-
-
-fs.readFileSync("lines").
-    toString().
-    split("\n").
-    forEach(line => console.log(line));
-
-
-fs.readFileSync("lines_odd").
-    toString().
-    split("\n").
-    filter((_, i) => i % 2 === 0).
-    forEach(line => console.log(line));
-
-fs.readFileSync("lines").
-    toString().
-    split("\n").
-    filter((_, i) => i % 2 === 0).
-    filter((_, i) => i >= 2 && i < 4).
-    forEach(line => console.log(line));
+    return (nums[index] ?? index) * 5;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,9 @@
+fn multiply(nums: Vec<usize>, index: usize) -> usize {
+    return nums.get(index).unwrap_or(&index) * 5;
+}
+
 fn main() {
-    let foo: Vec<_> = vec![1, 2 ,3]
-        .iter()
-        .map(|x| x + 1)
-        .collect();
+    let vec = vec![1, 2, 3, 4, 5];
 
-    println!("{:?}", foo);
-
-    let data = vec![1, 2, 3];
-    let mut bar = data
-        .iter()
-        .map(|x| x + 1);
-
-    let mut new_vector = vec![];
-
-    while let Some(x) = bar.next() {
-        new_vector.push(x)
-    }
-
-    println!("{:?}", foo);
-
-    let file = std::fs::read_to_string("lines").unwrap();
-
-    file
-        .lines()
-        .for_each(|line| println!("{}", line));
-
-    let file_odd = std::fs::read_to_string("lines_odd").unwrap();
-    file_odd
-        .lines()
-        .enumerate()
-        .filter(|(idx, _)| idx % 2 == 0)
-        .for_each(|(_, line)| println!("{}", line));
-
-    file
-        .lines()
-        .enumerate()
-        .filter(|(idx, _)| idx % 2 == 0)
-        .skip(2)
-        .take(2)
-        .for_each(|line| println!("{}", line.1));
-
+    println!("value: {}", multiply(vec, 0));
 }
