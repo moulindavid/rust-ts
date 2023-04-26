1cb0a3de2f7f1b0de5d88fc539ca4398af3c63e6
moulindavid <moulin.david@live.fr>
2023-04-26 18:49:34 +0200

    feat: borrow

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
+#[derive(Debug)]
+struct MyStruct {
+    age: usize,
+}
+
 fn main() {
-    let file_name = std::env::args()
-        .nth(1)
-        .expect("The file name to be passed in");
+    let item = MyStruct { age: 0 };
 
-    let file = std::fs::read_to_string(file_name).expect("unable to the file to string");
-
-    file.lines()
-        .filter_map(|line| line.parse::<usize>().ok())
-        .for_each(|line| println!("{}", line));
+    let items: Vec<&MyStruct> = vec![&item];
 }
